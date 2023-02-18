e3b1b7706af9ad89f0132dac8651b53806bf737f
HaofengLiang <31834884+HaofengLiang@users.noreply.github.com>
2023-02-17 22:50:19 -0800

    Integrated with Material UI.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Game from './components/tictactoe/game'
-import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-Amplify.configure(awsconfig);
+import Game from './components/tictactoe/game';
+import ResponsiveAppBar from './components/appBar/AppBar';
+import Button from '@mui/material/Button';
 
-function App({ signOut, user }) {
+// AWS Amplify for Authentication:
+// import { Amplify, Auth } from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+// Amplify.configure(awsconfig);
+
+function signOutHandler() {
+  console.log("Sign out!");
+};
+
+function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar/>
       <header className="App-header">
-        <h1>Hello {user.username}</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <Game/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,10 +30,9 @@ function App({ signOut, user }) {
         >
           Learn React
         </a>
-        <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);;
+export default App;;
